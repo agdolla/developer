6466b17f52303920194391fec1415d5f9f75cda9
Peer Richelsen <peeroke@richelsen.net>
2021-11-04 14:47:48 +0000

    pause/deprecate Open API till 2022

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import SwaggerUI from "swagger-ui-react";
+//import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import('buffer').then(({Buffer}) => {global.Buffer = Buffer;})
 
 export default function API() {
   return (
     <Layout title="API">
-      <SwaggerUI url="https://raw.githubusercontent.com/calendso/calendso/main/calendso.yaml" />
+      <div style={{ maxWidth: 700, margin: "40px auto", padding: 16 }}>
+        <h1>The Open API has been deprecated until 2022.</h1>
+        <p>Due to rapid changes to our internal API and lack of resources to keep the public-facing API up to date, 
+          we've decided to pause the official support for an exernal API.</p>
+          <p>
+            Please reach out to <a href="mailto:help@cal.com">help@cal.com</a> if you have a use-case that requires an open API and we may be able to help you out directly.
+          </p>
+      </div>
+      {/*
+        <SwaggerUI url="https://raw.githubusercontent.com/calendso/calendso/main/calendso.yaml" />
+      */}
     </Layout>
   );
 }
